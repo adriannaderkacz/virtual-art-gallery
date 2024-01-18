8f65ce9a95bffcc4cc7b5ad36fc5c696f848253d
DavouJ <djjobbi@hotmailcom>
2024-01-18 16:08:00 +0000

    added functionality to retrun the user search with content from the europeana db, only returns 12 results at the moment

--- Assets/js/display.js
+++ Assets/js/display.js
@@ -1,8 +1,63 @@
+/**
+ * 
+ * CODE CREATED BY: DAVOU JOBBI
+ * 
+ * CONTRIBUTIONS: AHMED IBRAHIM
+ * 
+ **/
+const harvardKey = "etticens"
+let results = []
+let tempResults
 
-$(document).ready(function(){
+/**
+ * 
+ * FUNCTION FOR FETCHING THE QUERIES FROM RELEVANT APIS
+ * 
+ **/
+function fetchingAPI(){
+    const userInput = JSON.parse(localStorage.getItem("search"))
+    console.log(userInput)
+    const harvardQuery = `https://api.europeana.eu/record/v2/search.json?wskey=${harvardKey}&query=what:("${userInput}")`
+    return fetch(harvardQuery)
+    .then(function(response){
+        return response.json()
+    }).then(function(data){
+        console.log(data)
+        temp = tempStoreData(data)
+        return temp
+    })
     
+}
 
-    displayData()
+/**
+ * 
+ * FUNCTION FOR TEMPORARILY STORING THE RESULTS OF THE..
+ * API FETCH TO DISPLAY ON SCREEN
+ * 
+ **/
+function tempStoreData(data){
+    for(let i=0;i<data.items.length;i++){
+        let creator = ""
+
+        try {
+            creator = data.items[i].dcCreator[0]
+        } catch (error) {
+            creator = "Unknown"
+        }
+
+        results[i] = { 
+            title: data.items[i].title[0],
+            artist: creator,
+            provider: data.items[i].provider[0],
+            image: data.items[i].edmIsShownBy[0]
+        }
+    }
+    console.log(results)
+
+    return results
+}
+
+displayData()
     /**
      * 
      * FUNCTION FOR DISPLAYING THE ARRAY OF ART PEICES..
@@ -18,8 +73,20 @@ $(document).ready(function(){
         }
     
     }
+
+$(document).ready(function(){
+    
+    
+    console.log(harvardKey)
+
+    tempResults = fetchingAPI()
+
+    tempResults.then(function test(result){
+        displayData()
+    })
+
+
+    
     
 })
-
-
 
--- Assets/js/fetch.js
+++ Assets/js/fetch.js
@@ -5,56 +5,21 @@
  * CONTRIBUTIONS: AHMED IBRAHIM
  * 
  **/
-const userSearch = $("#search")
-const userBtn = $("#search-button")
-const harvardKey = "etticens"
-let results = []
+const userSearch = $("#search-input")
 
-/**
- * 
- * FUNCTION FOR FETCHING THE QUERIES FROM RELEVANT APIS
- * 
- **/
-function fetchingAPI(){
-    const harvardQuery = `https://api.europeana.eu/record/v2/search.json?wskey=${harvardKey}&query=what:("${userSearch.val()}")`
 
-    fetch(harvardQuery)
-    .then(function(response){
-        return response.json()
-    }).then(function(data){
-        console.log(data)
-        tempStoreData(data)
-    })
-}
-
-/**
- * 
- * FUNCTION FOR TEMPORARILY STORING THE RESULTS OF THE..
- * API FETCH TO DISPLAY ON SCREEN
- * 
- **/
-function tempStoreData(data){
-    for(i=0;i<data.items.length;i++){
-        let creator = ""
-
-        try {
-            creator = data.items[i].dcCreator[0]
-        } catch (error) {
-            creator = "Unknown"
-        }
-
-        results[i] = { 
-            title: data.items[i].title[0],
-            artist: creator,
-            provider: data.items[i].provider
-        
-        }
-    }
-    console.log(results)
-}
-
-userBtn.on("click", function(e){
-    console.log('test')
+userSearch.on("keypress", function(e){
+    
     e.preventDefault()
-    fetchingAPI()
+    localStorage.setItem("search", JSON.stringify(userSearch.val()))
+    var key = e.which;
+    if(key == 13)  // the enter key code
+    {
+        window.location.href = "galleries.html"
+        
+        
+    }
+    
+    
 })
+
