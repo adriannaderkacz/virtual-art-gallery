3c2fd08e1f6ec4ec3b75f25abffb2fcb959761b9
ahmed-ibrahim20560 <ahmedibrahim20560@gmail.com>
2024-01-18 21:04:50 +0000

    added the harvard api data to the results object

--- Assets/js/display.js
+++ Assets/js/display.js
@@ -5,7 +5,8 @@
  * CONTRIBUTIONS: AHMED IBRAHIM
  * 
  **/
-const harvardKey = "etticens"
+const europeanaKey = "etticens"
+const harvardKey = "3e70e735-7cc4-493f-996e-a220833dd4a9"
 let results = []
 let tempResults
 const userSearch2 = $("#search-input")
@@ -15,19 +16,37 @@ const userSearch2 = $("#search-input")
  * FUNCTION FOR FETCHING THE QUERIES FROM RELEVANT APIS
  * 
  **/
-function fetchingAPI(){
+function fetchingAPI() {
     const userInput = JSON.parse(localStorage.getItem("search"))
     console.log(userInput)
-    const harvardQuery = `https://api.europeana.eu/record/v2/search.json?wskey=${harvardKey}&query=what:("${userInput}")`
-    return fetch(harvardQuery)
-    .then(function(response){
-        return response.json()
-    }).then(function(data){
-        console.log(data)
-        temp = tempStoreData(data)
-        return temp
-    })
-    
+    const europeanaQuery = `https://api.europeana.eu/record/v2/search.json?wskey=${europeanaKey}&query=what:("${userInput}")`
+    const harvardURL = `https://api.harvardartmuseums.org/object?apikey=${harvardKey}&title=${userInput}`;
+    fetch(europeanaQuery)
+        .then(function (response) {
+            return response.json()
+        }).then(function (data) {
+            console.log(data)
+            temp = tempStoreData(data)
+            return temp
+        })
+        .then(function (temp) {
+            fetch(harvardURL)
+                .then(function (response) {
+                    response.json()
+                })
+                .then(function (data) {
+
+                })
+        })
+        .then(function (temp) {
+            displayData()
+        }
+        )
+
+    // fetch(harvardURL)
+
+    // displayData()
+
 }
 
 /**
@@ -36,79 +55,89 @@ function fetchingAPI(){
  * API FETCH TO DISPLAY ON SCREEN
  * 
  **/
-function tempStoreData(data){
-    for(let i=0;i<data.items.length;i++){
+function tempStoreData(data) {
+    for (let i = 0; i < data.items.length; i++) {
         let creator = ""
         let img = ""
+        let tit = ""
+        let prov = ""
 
-        try {
-            creator = data.items[i].dcCreator[0]
-        } catch (error) {
-            creator = "Unknown"
+        if (data.items.apikey) {
+            prov = data.items[i].provider[0]
+            tit = data.items[i].title[0]
+            try {
+                creator = data.items[i].dcCreator[0]
+            } catch (error) {
+                creator = "Unknown"
+            }
+            try {
+                img = data.items[i].edmIsShownBy[0]
+            } catch (onerror) {
+                img = "./Assets/images/searchImgPlaceholder.jpeg"
+            }
+            img.onerror = function () {
+                img = "./Assets/images/searchImgPlaceholder.jpeg"
+            }
         }
-        try {
-            img = data.items[i].edmIsShownBy[0]
-        } catch (onerror) {
-            img = "./Assets/images/searchImgPlaceholder.jpeg"
+        else {
+            prov = data.records[i].creditline
+            tit = data.records[i].title
+            creator = data.records[i].persons[0].alphasort
+            img = data.records[i].images[0].baseimageurl
         }
-        img.onerror = function(){
-            img = "./Assets/images/searchImgPlaceholder.jpeg"
-        }
-        results[i] = { 
-            title: data.items[i].title[0],
+
+
+        results[i] = {
+            title: tit,
             artist: creator,
-            provider: data.items[i].provider[0],
+            provider: prov,
             image: img
+
         }
     }
-    console.log(results)
+    // console.log(results)
 
     return results
 }
 
-displayData()
-    /**
-     * 
-     * FUNCTION FOR DISPLAYING THE ARRAY OF ART PEICES..
-     * AND THEIR INFORMATION
-     * 
-     **/
-    function displayData(){
-    
-        for(let i=0;i<results.length;i++){
-            
-            $("img").eq(i).attr("src", results[i].image)
-            $("p").eq(i).text("Name: " + results[i].title + " Artist: " + results[i].artist)
-        }
-    
+// displayData()
+/**
+ * 
+ * FUNCTION FOR DISPLAYING THE ARRAY OF ART PEICES..
+ * AND THEIR INFORMATION
+ * 
+ **/
+function displayData() {
+
+    for (let i = 0; i < results.length; i++) {
+        console.log("tests")
+        $("img").eq(i).attr("src", results[i].image)
+        $("p").eq(i).text("Name: " + results[i].title + " Artist: " + results[i].artist)
     }
 
-$(document).ready(function(){
-    
-    
-    console.log(harvardKey)
+}
 
-    tempResults = fetchingAPI()
+$(document).ready(function () {
 
-    tempResults.then(function test(result){
-        displayData()
-    })
-    
+
+    console.log(europeanaKey)
+
+    fetchingAPI()
+
+
 })
-userSearch.on("keypress", function(e){
-    
+userSearch.on("keypress", function (e) {
+
     localStorage.setItem("search", JSON.stringify(userSearch2.val()))
     var key = e.which;
-    if(key == 13)  // the enter key code
+    if (key == 13)  // the enter key code
     {
-        tempResults = fetchingAPI()
+        fetchingAPI()
 
-        tempResults.then(function test(result){
-            displayData()
-        })
-        
+
+
     }
-    
-    
+
+
 })
 
