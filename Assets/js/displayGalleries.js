181ccb33940b1f9bd9b83fff3054c9974073c3ff
ahmed-ibrahim20560 <ahmedibrahim20560@gmail.com>
2024-01-21 11:12:16 +0000

    commented out added code to try and solve the issue of not being able to only remove one picture at a time but wasn't able to reach the solution

--- Assets/js/displayGalleries.js
+++ Assets/js/displayGalleries.js
@@ -22,14 +22,49 @@ $(document).ready(function () {
   $(".toUnsave").on("click", function () {
 
     var galleriesIndex = $(".toUnsave").index(this);
+    
 
     if (galleriesIndex >= 0 && galleriesIndex < galleriesArray.length) {
 
+
+      
       galleriesArray.splice(galleriesIndex, 1);
 
       localStorage.setItem("saved", JSON.stringify(galleriesArray));
 
+      
+
     }
   });
 });
+
+// $(document).ready(function () {
+//   displayGalleriesData();
+
+//   $(".toUnsave").on("click", function () {
+
+//     var galleriesIndex = $(".toUnsave").index(this);
+    
+
+//     if (galleriesIndex >= 0 && galleriesIndex < galleriesArray.length) {
+
+//       var inArray = galleriesArray.some(function(arrayItem) {
+//         return (
+//             arrayItem.title === galleriesArray[galleriesIndex].title &&
+//             arrayItem.artist === galleriesArray[galleriesIndex].artist &&
+//             arrayItem.provider === galleriesArray[galleriesIndex].provider &&
+//             arrayItem.image === galleriesArray[galleriesIndex].image
+//         )
+//     })
+
+//     if (inArray) {
+//       galleriesArray.splice(galleriesIndex, 1);
+
+//       localStorage.setItem("saved", JSON.stringify(galleriesArray));
+//     }
+  
+
+//     }
+//   });
+// });
 
